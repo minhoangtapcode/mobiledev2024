67d282ebc0bb8575b912a30ff06d088b735075b0
minhoangtapcode <minhhoangnguyen6688@gmail.com>
2024-09-13 10:58:04 +0700

    update practical 8 with viewpager

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ViewpagerAdapter.java
@@ -0,0 +1,36 @@
+package vn.edu.usth.usthweather;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+public class ViewpagerAdapter extends FragmentPagerAdapter {
+    private String[] tabTitles = new String[]{"HaNoi", "Singapore", "Beijing"};
+
+    public ViewpagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new WeatherAndForecastFragment();
+            case 1: return new WeatherAndForecastFragment();
+            case 2: return new WeatherAndForecastFragment();
+            default: return new WeatherAndForecastFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.usthweather;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -8,10 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
 
     private static final String TAG = "WeatherActivity";
+
+    private ViewpagerAdapter viewpagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +26,15 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate() called");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        ForecastFragment firstFragment = new ForecastFragment();
 
-        getSupportFragmentManager().beginTransaction().add(
-                    R.id.main, firstFragment).commit();
 
+        viewPager = findViewById(R.id.Viewpager);
+        viewpagerAdapter = new ViewpagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewpagerAdapter);
+
+
+        tabLayout = findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
